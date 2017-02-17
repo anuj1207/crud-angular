1d924142da78f32c4ea223731e4a7ecd183b29c4
raman <ramadeep.kaur@knoldus.in>
2017-02-17 18:42:42 +0530

    updated last

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,16 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {AppService} from "./app.singletonService";
+import {RouterModule} from "@angular/router";
+import {routes} from "./app.routes";
+import {HomeComponent} from "./home/home.component";
 
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule , RouterModule.forRoot(routes)],
+  declarations: [ AppComponent ,HomeComponent],
+  bootstrap:    [ AppComponent ],
+  providers:    [ AppService, ]
 })
 export class AppModule { }
